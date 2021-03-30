cbc674eea8059e3e2401a532e40fcd8ae0234adf
sigseg5 <36568961+sigseg5@users.noreply.github.com>
2021-03-30 23:02:57 +0500

    Added main script

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#JetBrains♥
+
+for product in IntelliJIdea WebStorm DataGrip PhpStorm CLion PyCharm GoLand RubyMine Rider; do
+  echo "Resetting trial period for $product"
+
+  echo "removing evaluation key..."
+  rm -rf /home/$USER/.config/JetBrains/$product*/eval
+
+  echo "removing all evlsprt properties in options.xml..."
+  sed -i "/evlsprt/d" /home/$USER/.config/JetBrains/$product*/options/other.xml
+
+  echo
+done
+
+echo "That's it, enjoy ;)"
+echo "Don't forget to buy licence when you can afford it"

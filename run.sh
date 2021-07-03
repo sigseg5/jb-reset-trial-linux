f8dad090eb26542805a76080106bbdcbd83a4cf0
sigseg5 <36568961+sigseg5@users.noreply.github.com>
2021-07-03 18:03:43 +0500

    Fixes for modern IDEs version

--- run.sh
+++ run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 #JetBrains♥
 
+# Remove data from `.java/.userPerfs/jetbrains
+
 for product in IntelliJIdea WebStorm DataGrip PhpStorm CLion PyCharm GoLand RubyMine Rider; do
   echo "Resetting trial period for $product"
 
   echo "removing evaluation key..."
   rm -rf /home/$USER/.config/JetBrains/$product*/eval
 
-  echo "removing all evlsprt properties in options.xml..."
-  sed -i "/evlsprt/d" /home/$USER/.config/JetBrains/$product*/options/other.xml
+#  echo "removing all evlsprt properties in options.xml..."
+#  sed -i "/evlsprt/d" /home/$USER/.config/JetBrains/$product*/options/other.xml
 
   echo
 done

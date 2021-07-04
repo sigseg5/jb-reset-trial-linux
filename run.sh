739c816821a2a3a5eabf650ce48a9be848d53cdd
sigseg5 <36568961+sigseg5@users.noreply.github.com>
2021-07-05 00:18:04 +0500

    Final fixes for modern IDEs version

--- run.sh
+++ run.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 #JetBrains♥
 
-# Remove data from `.java/.userPerfs/jetbrains
-
 for product in IntelliJIdea WebStorm DataGrip PhpStorm CLion PyCharm GoLand RubyMine Rider; do
   echo "Resetting trial period for $product"
+
+  echo "Closing $product"
+  pkill -f /usr/share/$product*
 
   echo "removing evaluation key..."
   rm -rf /home/$USER/.config/JetBrains/$product*/eval
 
-#  echo "removing all evlsprt properties in options.xml..."
-#  sed -i "/evlsprt/d" /home/$USER/.config/JetBrains/$product*/options/other.xml
-
   echo
 done
+
+echo "Removing regdata..."
+folders=$( ls -d /home/$USER/.java/.userPrefs/jetbrains/*/ )
+base_path=$( ls -d /home/$USER/.java/.userPrefs/jetbrains/ )
+len=${#base_path}
+
+for i in ${folders[@]}; do
+  folder_name=${i:$len}
+  if [[ "$folder_name" != \_* ]]; then
+
+    if [ $folder_name != "jetprofile/" ]; then
+
+      if [ $folder_name != "syncsettings/" ]; then
+        echo "removing regdata from $folder_name..."
+        rm -rf $i*/
+        # for debug:
+        # rm -r -v -i $i*/ 
+      fi
+
+    fi
+
+  fi
+done
+
 
 echo "That's it, enjoy ;)"
 echo "Don't forget to buy licence when you can afford it"
